fd053730abb9c7b256c66d7d245542a0a787497b
순민 <tkksm2@gmail.com>
2022-08-07 16:52:07 +0900

    일곱 난쟁이!

---
+++ 알고리즘공부/기본문제/일곱 난쟁이/index.js
@@ -0,0 +1,12 @@
+function solution(arr){
+  let sum = arr.reduce((acc, cur)=>acc+cur, 0);
+  let target = sum - 100;
+  for(let i = 0; i < arr.length; i++){
+    if((target - arr[i] !== arr[i]) && arr.includes(target - arr[i])){
+      return arr.filter((item)=> !(item === target - arr[i] || item === arr[i]));
+    }
+  }
+}
+
+console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13]));
+
