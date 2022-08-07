6292ef8f68e353c494dc594031957a961bdce83b
순민 <tkksm2@gmail.com>
2022-08-07 17:22:36 +0900

    대소문자 변환 다른 방법

--- 알고리즘공부/기본문제/대소문자변환.js
+++ 알고리즘공부/기본문제/대소문자변환.js
@@ -1,15 +1,21 @@
 
 const solution = (s) => {
+  // let ans = ""
+  // for(let x of s){
+  //   let num = x.charCodeAt();
+  //   if(num >= 97 && num <= 122){
+  //     ans += String.fromCharCode(num - 32)
+  //   }else{
+  //     ans += String.fromCharCode(num + 32)
+  //   }
+  // }
+  // return ans;
   let ans = ""
   for(let x of s){
-    let num = x.charCodeAt();
-    if(num >= 97 && num <= 122){
-      ans += String.fromCharCode(num - 32)
-    }else{
-      ans += String.fromCharCode(num + 32)
-    }
+    if(x === x.toUpperCase()) ans += x.toLowerCase();
+    else ans += x.toUpperCase();
   }
-  return ans;
+  return ans
 }
 
 console.log(solution('StuDY'))
