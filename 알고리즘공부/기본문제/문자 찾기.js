8b15d3090dffafa7aa39450cdfdf65fddf388e32
순민 <tkksm2@gmail.com>
2022-08-07 17:10:35 +0900

    문자 찾기

--- 알고리즘공부/기본문제/문자 찾기.js
+++ 알고리즘공부/기본문제/문자 찾기.js
@@ -1,7 +1,11 @@
 
-const solution = (s,t) =>{
-  return s.split("").filter((al)=>al===t).length
+const solution = (s) =>{
+  let answer = 0;
+  for(let st of s){
+    if(st.charCodeAt(0) === st.toUpperCase().charCodeAt(0)) answer++
+  }
+  return answer;
 }
 
 
-console.log(solution("COMPUTERPROGRAMMING","R"))
+console.log(solution("KoreaTimeGood"))
