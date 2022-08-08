206c18d56b9a4606030dde67aaa69ec35143100f
순민 <tkksm2@gmail.com>
2022-08-08 20:29:53 +0900

    보이는 학생(다시보기)

---
+++ 알고리즘공부/2차원 배열 탐색/보이는 학생.js
@@ -0,0 +1,29 @@
+// const solution = (arr) => {
+//   let answer = 1;
+//   for(let i=1; i<arr.length; i++){
+//     if(arr[i] > arr[i-1]){
+//       answer++;
+//     } else{
+//       for(let j=i+1; j<arr.length; j++){
+//         if(arr[j] > arr[i-1]){
+//           answer++;
+//           i=j;
+//           break;
+//         } 
+//       }
+//     }
+//   }
+//   return answer;
+// }
+const solution = (arr) => {
+  let answer = 1;
+  max = arr[0];
+  for(let i=1; i<arr.length;i++){
+    if(arr[i] > max){
+      answer++;
+      max = arr[i]
+    }
+  }
+  return answer;
+}
+console.log(solution([130,135,148,140,145,150,150,153]))
