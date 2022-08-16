7c928afb596e5e918d2dd79e82051c15c94c4c41
순민 <tkksm2@gmail.com>
2022-08-16 22:25:02 +0900

    연속 부분수열1

--- 알고리즘공부/효율성/연속 부분수열1.js
+++ 알고리즘공부/효율성/연속 부분수열1.js
@@ -17,5 +17,20 @@
 //   return result;
 // };
 
+const solution = (arr, m) => {
+  let answer = 0;
+  let lt = 0;
+  let sum = 0;
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    if (sum === m) answer++;
+    while (sum >= m) {
+      sum -= arr[lt++];
+      if (sum === m) answer++;
+    }
+  }
+  return answer;
+};
+
 let arr = [1, 2, 1, 3, 1, 1, 1, 2];
 console.log(solution(arr, 6));
