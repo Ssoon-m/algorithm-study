514c20ac70671fdde961c615a0232410751ad52b
순민 <tkksm2@gmail.com>
2022-08-17 22:20:53 +0900

    최대 매출(Sliding Window)-강의풀이

--- 알고리즘공부/효율성/최대 매출.js
+++ 알고리즘공부/효율성/최대 매출.js
@@ -6,18 +6,30 @@
 // 최대 매출액은 11,20,25  = 56이다.
 
 // 내가 푼 풀이
+// const solution = (k, arr) => {
+//   const result = [];
+//   let lt = 0;
+//   for (let rt = k; rt <= arr.length; rt++) {
+//     let sum = 0;
+//     for (let lt2 = lt; lt2 < rt; lt2++) {
+//       sum += arr[lt2];
+//     }
+//     lt++;
+//     result.push(sum);
+//   }
+//   return result;
+// };
+
 const solution = (k, arr) => {
-  const result = [];
-  let lt = 0;
-  for (let rt = k; rt <= arr.length; rt++) {
-    let sum = 0;
-    for (let lt2 = lt; lt2 < rt; lt2++) {
-      sum += arr[lt2];
-    }
-    lt++;
-    result.push(sum);
+  let answer = 0;
+  let sum = 0;
+  for (let i = 0; i < k; i++) sum += arr[i];
+  answer = sum;
+  for (let i = k; i < arr.length; i++) {
+    sum += arr[i] - arr[i - k];
+    answer = Math.max(answer, sum);
   }
-  return result;
+  return answer;
 };
 
 let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
