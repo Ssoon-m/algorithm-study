8ec7c8ad67cda89a35c49c5cda5aaad074e2c4e6
순민 <tkksm2@gmail.com>
2022-08-22 19:33:33 +0900

    아나그램(해쉬)-강의풀이

--- 알고리즘공부/효율성/아나그램(해쉬).js
+++ 알고리즘공부/효율성/아나그램(해쉬).js
@@ -1,22 +1,36 @@
-const solution = (arr1, arr2) => {
-  const obj1 = arr1.split('').reduce((acc, cur) => {
-    acc[cur] = acc[cur] || 0;
-    acc[cur]++;
-    return acc;
-  }, {});
-  const obj2 = arr2.split('').reduce((acc, cur) => {
-    acc[cur] = acc[cur] || 0;
-    acc[cur]++;
-    return acc;
-  }, {});
-  let flag = false;
-  for (const [key, value] of Object.entries(obj1)) {
-    if (obj1[key] !== obj2[key]) flag = true;
+// const solution = (arr1, arr2) => {
+//   const obj1 = arr1.split('').reduce((acc, cur) => {
+//     acc[cur] = acc[cur] || 0;
+//     acc[cur]++;
+//     return acc;
+//   }, {});
+//   const obj2 = arr2.split('').reduce((acc, cur) => {
+//     acc[cur] = acc[cur] || 0;
+//     acc[cur]++;
+//     return acc;
+//   }, {});
+//   let flag = false;
+//   for (const [key, value] of Object.entries(obj1)) {
+//     if (obj1[key] !== obj2[key]) flag = true;
+//   }
+//   return flag ? 'NO' : 'YES';
+// };
+
+const solution = (str1, str2) => {
+  let answer = 'YES';
+  let sH = new Map();
+  for (let x of str1) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
   }
-  return flag ? 'NO' : 'YES';
+  for (let x of str2) {
+    if (!sH.has(x) || sH.get(x) === 0) return 'NO';
+    sH.set(x, sH.get(x) - 1);
+  }
+  return answer;
 };
 
-const arr1 = 'AbaAeCe';
-const arr2 = 'baeeACA';
+const str1 = 'AbaAeCe';
+const str2 = 'baeeACA';
 
-console.log(solution(arr1, arr2));
+console.log(solution(str1, str2));
