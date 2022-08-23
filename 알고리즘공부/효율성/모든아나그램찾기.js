56fedb5e7e7ee45d8a0408fda040eefc2bb5c03d
순민 <tkksm2@gmail.com>
2022-08-23 22:51:57 +0900

    모든아나그램찾기(다시풀기1)

--- 알고리즘공부/효율성/모든아나그램찾기.js
+++ 알고리즘공부/효율성/모든아나그램찾기.js
@@ -1,21 +1,19 @@
 const compareMaps = (map1, map2) => {
-  // 둘이 아나그램 이라면 당연히 key의 사이즈가 같아야함.
   if (map1.size !== map2.size) return false;
-  for (let [key, val] of map1) {
-    if (!map2.has(key) || map2.get(key) !== val) return false;
+  for (let [key, value] of map1) {
+    if (!map2.has(key) || map2.get(key) !== value) return false;
   }
   return true;
 };
 const solution = (s, t) => {
-  let answer = 0;
-  let tH = new Map();
-  let sH = new Map();
+  let result = 0;
+  const sH = new Map();
+  const tH = new Map();
   for (let x of t) {
     if (tH.has(x)) tH.set(x, tH.get(x) + 1);
     else tH.set(x, 1);
   }
-  let len = t.length - 1;
-  // sliding window 를 위한 기준점을 하나 만든다.
+  const len = t.length - 1;
   for (let i = 0; i < len; i++) {
     if (sH.has(s[i])) sH.set(s[i], sH.get(s[i]) + 1);
     else sH.set(s[i], 1);
@@ -26,13 +24,13 @@ const solution = (s, t) => {
     if (sH.has(s[rt])) sH.set(s[rt], sH.get(s[rt]) + 1);
     else sH.set(s[rt], 1);
     // 비교
-    if (compareMaps(sH, tH)) answer++;
-    // 빼고
+    if (compareMaps(sH, tH)) result++;
+    // 빼기
     sH.set(s[lt], sH.get(s[lt]) - 1);
     if (sH.get(s[lt]) === 0) sH.delete(s[lt]);
     lt++;
   }
-  return answer;
+  return result;
 };
 
 // 아나그램은 문자의 구성요소만 똑같으면 된다.
