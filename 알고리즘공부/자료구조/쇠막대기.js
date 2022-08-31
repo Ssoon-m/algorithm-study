abea0c7232788345aa9353e0be310a84ff8ec974
순민 <tkksm2@gmail.com>
2022-08-31 23:24:19 +0900

    쇠막대기(다시풀기)

---
+++ 알고리즘공부/자료구조/쇠막대기.js
@@ -0,0 +1,20 @@
+// 9시 55
+const solution = (str) => {
+  let result = 0;
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(') stack.push(str[i]);
+    else {
+      stack.pop();
+      if (str[i - 1] === '(') result += stack.length;
+      else result += 1;
+    }
+  }
+  return result;
+};
+
+const str = '()(((()())(())()))(())';
+// 답은 17이 나와야 한다.
+// (((()(()()))(())()))(()())
+// 위는 24
+console.log(solution('(((()(()()))(())()))(()())'));
