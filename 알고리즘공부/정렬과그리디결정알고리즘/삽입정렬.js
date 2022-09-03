0a923fca636f842ffafb0b0bb5fa32ebcfe6898d
순민 <tkksm2@gmail.com>
2022-09-03 17:44:44 +0900

    삽입정렬(다른풀이)

--- 알고리즘공부/정렬과그리디결정알고리즘/삽입정렬.js
+++ 알고리즘공부/정렬과그리디결정알고리즘/삽입정렬.js
@@ -2,7 +2,7 @@
 const solution = (arr) => {
   let answer = arr;
   for (let i = 0; i < arr.length; i++) {
-    let tmp = arr[i];
+    let tmp = arr[i]; //11
     let j = 0;
     for (j = i - 1; j >= 0; j--) {
       if (arr[j] > tmp) arr[j + 1] = arr[j];
@@ -12,6 +12,18 @@ const solution = (arr) => {
   }
   return answer;
 };
+
+// function solution(arr) {
+//   let answer = arr;
+//   for (let i = 1; i < answer.length; i++) {
+//     for (let j = i; j >= 0; j--) {
+//       if (answer[j] < answer[j - 1])
+//         [answer[j], answer[j - 1]] = [answer[j - 1], answer[j]];
+//       else break;
+//     }
+//   }
+//   return answer;
+// }
 
 const arr = [11, 7, 5, 6, 10, 9];
 console.log(solution(arr));
