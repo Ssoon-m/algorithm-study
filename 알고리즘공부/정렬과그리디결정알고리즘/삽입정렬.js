116ba47a4720c364a589a25874f5218e4ab5ad57
순민 <tkksm2@gmail.com>
2022-09-03 16:51:57 +0900

    삽입정렬

---
+++ 알고리즘공부/정렬과그리디결정알고리즘/삽입정렬.js
@@ -0,0 +1,17 @@
+//
+const solution = (arr) => {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = arr[i];
+    let j = 0;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) arr[j + 1] = arr[j];
+      else break;
+    }
+    arr[j + 1] = tmp;
+  }
+  return answer;
+};
+
+const arr = [11, 7, 5, 6, 10, 9];
+console.log(solution(arr));
