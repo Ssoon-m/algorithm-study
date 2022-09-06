0eed1a2c394d585d2995e6cd20412e2e2436a42b
순민 <tkksm2@gmail.com>
2022-09-06 22:03:23 +0900

    이분검색(다시풀기)

---
+++ 알고리즘공부/정렬과그리디결정알고리즘/이분검색.js
@@ -0,0 +1,20 @@
+//
+const solution = (arr, target) => {
+  let answer = 0;
+  arr.sort((a, b) => a - b);
+  let lt = 0;
+  let rt = arr.length - 1;
+  while (lt <= rt) {
+    let mid = Math.floor((lt + rt) / 2);
+    if (arr[mid] === target) {
+      answer = mid + 1;
+      break;
+    } else if (arr[mid] > target) rt = mid - 1;
+    else lt = mid + 1;
+  }
+  return answer;
+};
+
+const arr = [23, 87, 65, 12, 57, 32, 99, 81];
+const target = 32;
+console.log(solution(arr, target));
