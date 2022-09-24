2655bf99ab009a6398a0290883cece59be68092d
순민 <tkksm2@gmail.com>
2022-09-24 16:06:06 +0900

    재귀함수와스택프레임(강의풀이)

--- 알고리즘공부/재귀함수와완전탐색(DFS:깊이우선탐색)/재귀함수와스택프레임.js
+++ 알고리즘공부/재귀함수와완전탐색(DFS:깊이우선탐색)/재귀함수와스택프레임.js
@@ -1,10 +1,13 @@
 // 자연수 N이 입력되면 재귀함수를 이용하여 1부터 N까지를 출력하는 프로그램을 작성하세요.
-const solution = (N) => {
-  if (N === 0) return;
-  else {
-    solution(N - 1);
-    console.log(N);
-  }
+const solution = (n) => {
+  const DFS = (L) => {
+    if (L === 0) return;
+    else {
+      DFS(L - 1);
+      console.log(L);
+    }
+  };
+  DFS(n);
 };
 
 let N = 3;
